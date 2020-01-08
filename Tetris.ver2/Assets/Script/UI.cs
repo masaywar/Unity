ad5584c44788073d2df566154726f976c6a555ba
Lee <dngus0715@naver.com>
2020-01-09 00:42:17 +0900

    Update

--- Tetris.ver2/Assets/Script/UI.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UI : MonoBehaviour
-{
-    // Start is called before the first frame update
-    
-}
